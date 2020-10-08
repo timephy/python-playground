1b26653afd2b1a50bf8e621574c8de509a11cf8a
Tim Guggenmos <tim.guggenmos@me.com>
2020-10-08 16:27:56 +0200

    read_stream_nonblocking: refactor to python3.8 syntax

--- read_stream_nonblocking/__main__.py
+++ read_stream_nonblocking/__main__.py
@@ -8,9 +8,16 @@ nbsr = NonBlockingStreamReader(sys.stdin)
 
 
 while True:
-    output = nbsr.readline()  # no timeout arg will not block
-    if output:
-        print(f'if output: {output=}')
-    else:
-        print(f'if not output: {output=}')
+    # output = nbsr.readline()  # no timeout arg will not block
+    # if output:
+    #     print(f'if output: {output=}')
+    # else:
+    #     print(f'if not output: {output=}')
+
+    # Python 3.8 style variant with 'walrus operator'.
+    # Reads all lines after another, does not wait for another 'while True'
+    while output := nbsr.readline():
+        print(f'while output: {output=}')
+
+    print('sleeping 1s')
     time.sleep(1)  # sleep for usability in cli
